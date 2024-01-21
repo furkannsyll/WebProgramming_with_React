6ec01e498ea1ecd9daabc3af8de677b948701c41
Furkan Soylu <fsoylu567@gmail.com>
2024-01-22 00:28:58 +0300

    working with Bootstrap

--- intro/src/App.js
+++ intro/src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import Navi from './Navi';
 import CategoryList from './CategoryList';
 import ProductList from './ProductList';
+import { Col, Container, Row } from 'reactstrap';
 
 function App() {
   return (
     <div>
-      <Navi></Navi>
-      <CategoryList></CategoryList>
-      <ProductList></ProductList>
+      <Container>
+        <Row>
+          <Navi></Navi>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList></CategoryList>
+          </Col>
+          <Col xs="9">
+            <ProductList></ProductList>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
