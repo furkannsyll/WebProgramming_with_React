aa7337608ec69b049cd5a11cae08067c6a2dd379
Furkan Soylu <fsoylu567@gmail.com>
2024-01-22 18:44:19 +0300

    working with event

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -8,8 +8,12 @@ export default class CategoryList extends Component {
             categories: [
                 { catrgoryId: 1, categoryName: "Beverages" },
                 { catrgoryId: 2, categoryName: "Condiments" }
-            ]
+            ],
+            currentCategory: ""
         };
+        this.changeCategory = (category) => {
+            this.setState({ currentCategory: category.categoryName })
+        }
     }
     render() {
         return (
@@ -18,10 +22,13 @@ export default class CategoryList extends Component {
                 <ListGroup>
                     {
                         this.state.categories.map(category => (
-                            <ListGroupItem key={category.catrgoryId}>{category.categoryName}</ListGroupItem>
+                            <ListGroupItem onClick={() => this.changeCategory(category)} key={category.catrgoryId}>
+                                {category.categoryName}
+                            </ListGroupItem>
                         ))
                     }
                 </ListGroup>
+                <h4>{this.state.currentCategory}</h4>
             </div>
         )
     }
