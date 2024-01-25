11322098bc56cdd5df4ad167ebd8dd0a7e9046bc
Furkan Soylu <fsoylu567@gmail.com>
2024-01-25 23:49:09 +0300

    Working with Multiple Form elements

--- intro/src/FormDemo1.js
+++ intro/src/FormDemo1.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 
 export default class FormDemo1 extends Component {
-    state = { userName: '' }
+    state = { userName: '', city: '' }
     onChangeHandler = (event) => {
-        this.setState({ userName: event.target.value })
+        let name = event.target.name;
+        let value = event.target.value;
+
+        this.setState({[name]:value});
     }
 
     onSubmitHandler = (event) => {
@@ -16,8 +19,13 @@ export default class FormDemo1 extends Component {
             <div>
                 <form onSubmit={this.onSubmitHandler}>
                     <h3>User Name</h3>
-                    <input type='text' onChange={this.onChangeHandler}></input>
+                    <input name='userName' type='text' onChange={this.onChangeHandler}></input>
                     <h3>User Name is {this.state.userName}</h3>
+
+                    <h3>City</h3>
+                    <input name='city' type='text' onChange={this.onChangeHandler}></input>
+                    <h3>City is {this.state.city}</h3>
+
                     <input type='submit' value='Save'></input>
                 </form>
             </div>
